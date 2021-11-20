23bccbda8e04796614c7df446fc7b2f90156a92f
Rodrigo Arana <def.zerocode@gmail.com>
2021-11-19 16:14:51 -0800

    Corey's second class done

--- python/practicing/oop.py
+++ python/practicing/oop.py
@@ -1,6 +1,9 @@
 def run():
     
     class Employee:
+    
+        num_of_emps = 0
+        raise_amount = 1.04
         
         def __init__(self, first, last, pay):
             self.first = first
@@ -8,22 +11,34 @@ def run():
             self.pay = pay
             self.email = first + '.' + last + '@company.com'
 
+            Employee.num_of_emps += 1            
+
         def fullname(self):
             return '{} {}'.format(self.first, self.last)
+
+        def apply_raise(self):
+            self.pay = int(self.pay * self.raise_amount)
+
+    print(Employee.num_of_emps)
+
 
     emp_1 = Employee('Rodrigo', 'Arana', 50000)
     emp_2 = Employee('Test', 'User', 60000)
 
-    # print(emp_1)
-    # print(emp_2)
+    print(Employee.num_of_emps)
 
-    print(emp_1.email)
-    print(emp_2.email)
+    # emp_1.raise_amount = 1.05
 
-    print(emp_1.fullname())
-    print(emp_2.fullname())
+    # print(emp_1.__dict__)
+    # print(vars(emp_2))
 
-    print(Employee.fullname(emp_1))
+    # print(Employee.raise_amount)
+    # print(emp_1.raise_amount)
+    # print(emp_2.raise_amount)
+    
+    # emp_1.apply_raise()
+
+    
 
 if __name__ == '__main__':
     run()
